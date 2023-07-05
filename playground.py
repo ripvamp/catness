ebaa7c30d4e5cbad546a78f2b8cedbb4347eb84e
dazzie2load <79106393+dazziedez@users.noreply.github.com>
2023-07-06 01:26:43 +0200

    Update playground.py

--- playground.py
+++ playground.py
@@ -1,23 +1,31 @@
 import discord
 import asyncio
+
 from discord.ext import commands
 
-intents = discord.Intents.all()
-from data.__init__ import config
+from data import config
 
-
-class Client(commands.Bot):
-    def __init__(self):
-        super().__init__(intents=intents, command_prefix=config["prefix"])
+class Client(commands.AutoShardedBot):
+    def __init__(self, intents, command_prefix, allowed_mentions):
+        super().__init__(
+            intents=intents,
+            command_prefix=command_prefix,
+            allowed_mentions=allowed_mentions
+        )
         self.command_prefix = config["prefix"]
 
-ce = Client()
+
+intents = discord.Intents.all()
+prefix = config["prefix"]
+mentions = discord.AllowedMentions(roles=False, users=True, everyone=False)
+bot = Client(intents=intents, command_prefix=prefix, allowed_mentions=mentions)
+
 
 async def load():
-    await ce.load_extension('jishaku')
-    await ce.load_extension('cogs.events')
+    await bot.load_extension('jishaku')
+    await bot.load_extension('cogs.events')
     print('🟪 initial extensions loaded')
 
 
 asyncio.run(load())
-ce.run(config["TOKEN"])
+bot.run(config["TOKEN"])
